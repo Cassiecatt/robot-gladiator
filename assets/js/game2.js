88ab0f89072bcccf60af6f25fcdae5acb250ae11
cassie.cattabiani@gmail.com <cassie.cattabiani@gmail.com>
2020-06-13 12:46:11 -0700

    randomized

--- assets/js/game2.js
+++ assets/js/game2.js
@@ -33,9 +33,17 @@ var fightOrSkip = function() {
 
 
 var fight = function(enemy) {
-      // repeat and execute as long as the enemy robot is alive 
+  // keep track of who goes first
+  var isPlayerTurn = true;
+
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+
   while(playerInfo.health > 0 && enemy.health > 0) {
-      //ask the user if they'd like to fight or skip using fightOrSkip funcion
+    if (isPlayerTurn) {
+    //ask the user if they'd like to fight or skip using fightOrSkip funcion
     if (fightOrSkip()) {
       // if true, leave fight by breaking loop
       break;
@@ -62,9 +70,11 @@ var fight = function(enemy) {
       } else {
       window.alert(enemy.name + " still has " + enemy.health + " health left.");
     }
-
+    // player gets attacked first
+  } else {
     // generate random damage value based on enemy's attack power
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
+    
     // remove player's health by subtracting the amount set in the enemy.attack variable
     playerInfo.health = Math.max(0, playerInfo.health - damage);
     console.log(
@@ -79,9 +89,12 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
     }
   }
+  // switch turn order for next round
+  isPlayerTurn = !isPlayerTurn;
+  }
 };
 
-// functino to start a new game
+// function to start a new game
 var startGame = function() {
    //reset player stats
    playerInfo.reset();
